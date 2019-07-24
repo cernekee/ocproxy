5876cf460b248e53548b7aa37d0ee58f024fed81
Raphael Groner <raphgro@fedoraproject.org>
2019-07-24 23:16:36 +0200

    drop useless files copied accidently from lwip project

--- lwip/contrib/ports/unix/example_app/CMakeFiles/lwipdocs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lwipdocs"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lwipdocs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
